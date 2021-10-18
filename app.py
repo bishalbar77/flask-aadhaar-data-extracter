4503ad0b9779008e24b3ca0f7fecc910969301dc
bishalbar77 <bishalranabr7@gmail.com>
2021-10-18 06:27:54 +0530

    deploy on heroku

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+# from aadhaar.qr import AadhaarSecureQR
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+@app.route('/aadhaar/<int:qr>')
+def get_data(qr):
+    # secure_qr = AadhaarSecureQR(qr)
+    # data = secure_qr.extract_data()
+    return str(qr)
+
+if __name__ == "__main__":
+    app.run(debug=True)
